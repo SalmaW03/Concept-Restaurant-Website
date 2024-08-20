fd72c279a6a89152c6991ba96e747ceec8c09870
SalmaW03 <107649425+SalmaW03@users.noreply.github.com>
2024-08-21 01:49:31 +0400

    Add files via upload

---
+++ restaurant website/footer.js
@@ -0,0 +1,186 @@
+class myHeader extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+        <header>
+            <h1 class="title">
+                <a href="index.html" class="head">CROÛTE</a>
+            </h1>
+        </header>
+        `
+    }
+}
+
+customElements.define('my-header', myHeader)
+
+class myFooter extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+        <footer id="hours">
+            <div>
+                <h2 class="title2">
+                    <a href="index.html" class="head">CROÛTE</a>
+                </h2>
+                <p class="copyright">All Rights Reserved to Croûte 2024</p>
+
+                <div class="cover">
+                <h5>LOCAL ARTISAN PIZZA MADE TO ORDER</h5>
+                <div class="area1">
+                    <span class="area2">City, Dubai</span>
+                    <span class="area2">0000000000</span>
+                </div>
+                <div class="area1">
+                    <span class="area2">City, Sharjah</span>
+                    <span class="area2">0000000000</span>
+                </div>
+                    <p class="time">Open everyday from 10 am to 10 pm</p>
+                </div>
+
+                <img class="svg" src="images/instagram-socials-social-media-icon-svgrepo-com.svg">
+            </div>
+        </footer>
+    `
+    }
+};
+
+customElements.define('my-footer', myFooter);
+
+
+class myNav extends HTMLElement {
+    connectedCallback () {
+        this.innerHTML = `
+        <div class="none">
+            <div id="customID" class="cta">
+            <h1>
+                <a href="index.html" class="logo">CROÛTE</a>
+            </h1>
+                <nav>
+                    <ul class="nav1">   
+                        <li class="nav2">
+                            <a href="menu.html" class="cta-1">MENU</a>
+                        </li>
+                        <li class="nav2">
+                            <a href="order.html" class="cta-1">ORDER</a>
+                        </li>
+                        <li class="nav2">
+                            <a href="#hours" class="cta-1">HOURS & LOCATION</a>
+                        </li>
+                        <li class="nav2">
+                            <a href="gallery.html" class="cta-1">GALLERY</a>
+                        </li>
+                        <li class="nav2">
+                            <a href="#" class="cta-1">OUR STORY</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+
+        <div class="none2">
+        <div id="customID2" class="cta">
+            <h1>
+                <a href="index.html" class="logo2">CROÛTE</a>
+            </h1>
+            <div id="hamburger-icon">
+            <input type="checkbox" id="menu-button">
+            <label for="menu-button">
+            <nav>
+            <div class="ham-menu">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+            </nav>
+            </label>
+            <div class="menu-1">
+                <ul class="mobile-menu ul-1">
+                    <li class="li-1">
+                        <a href="index.html" class="mobile">Home</a>
+                    </li>
+                    <li class="li-1">
+                        <a href="menu.html" class="mobile">Menu</a>
+                    </li>
+                    <li class="li-1">
+                        <a href="order.html" class="mobile">Order</a>
+                    </li>
+                    <li class="li-1">
+                        <a href="#hours" class="mobile">Hours & Location</a>
+                    </li>
+                    <li class="li-1">
+                        <a href="gallery.html" class="mobile">Gallery</a>
+                    </li>
+                    <li class="li-1">
+                        <a href="#" class="mobile">Our Story</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </div>
+        `
+    }
+}
+
+customElements.define('my-nav', myNav)
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener ("scroll", () => {
+    const currentScroll = window.pageYOffset
+
+    if (currentScroll <= 0) {
+        body.classList.remove("scrollUp")
+    }
+
+    if (currentScroll > lastScroll && !body.classList.contains("scrollDown")) {
+        body.classList.remove('scrollUp')
+        body.classList.add("scrollDown")
+    }
+
+    if (currentScroll < lastScroll && body.classList.contains("scrollDown")) {
+        body.classList.remove('scrollDown')
+        body.classList.add("scrollUp")
+    }
+
+    if (lastScroll = 0) {
+        body.classList.remove("scrollUp")
+    }
+
+    lastScroll = currentScroll;
+
+})
+
+myID = document.getElementById("customID");
+
+let myScrollFunc = function() {
+    var y = window.scrollY;
+    if (y >= 10) {
+        myID.className = "cta show"
+    } else {
+        myID.className = "cta hide-1"
+    }
+};
+
+window.addEventListener("scroll", myScrollFunc);
+
+
+myID2 = document.getElementById("customID2");
+
+let myScrollFuncc = function() {
+    var y = window.scrollY;
+    if (y >= 10) {
+        myID2.className = "cta show"
+    } else {
+        myID2.className = "cta hide-1"
+    }
+};
+
+window.addEventListener("scroll", myScrollFuncc);
+
+const hamMenu = document.querySelector(".ham-menu");
+
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+
+    document.body.classList.toggle("hide1");
+})
---
+++ restaurant website/script.js
@@ -0,0 +1,83 @@
+function zoom(imgLink) {
+    document.getElementById("fullscreen").src = imgLink;
+    document.getElementById("full-screen-view").style.display = "block";
+
+    document.body.classList.add("hide");
+}
+
+function closeZoom() {
+    document.getElementById("full-screen-view").style.display = "none";
+
+    document.body.classList.remove("hide");
+}
+
+const target = document.getElementById("target");
+target.addEventListener("click", () => {
+    /*console.log("its clicked")*/
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/person-holding-a-slice-of-pizza-4109111/" class="demo1" target="blank" rel="external nofollow">Image created by Polina Tankilevitch - Pexels</a>` ;
+})
+
+const target1 = document.getElementById("target1");
+target1.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/overhead-shot-of-anchovies-on-pieces-of-bread-5602610/" class="demo1" target="blank" rel="external nofollow">Image created by Leeloo The First - Pexels</a>` ;
+})
+
+const target2 = document.getElementById("target2");
+target2.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/truffles-on-pizza-20117232/" class="demo1" target="blank" rel="external nofollow">Image created by Yusuf Kaya - Pexels</a>` ;
+})
+
+const target3 = document.getElementById("target3");
+target3.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/salad-with-roast-peach-on-plate-24018365/" class="demo1" target="blank" rel="external nofollow">Image created by Vanessa Pozos - Pexels</a>` ;
+})
+
+const target4 = document.getElementById("target4");
+target4.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/glass-of-fresh-cocktail-on-white-background-4021995/" class="demo1" target="blank" rel="external nofollow">Image created by Karolina Kaboompics - Pexels</a>` ;
+})
+
+const target5 = document.getElementById("target5");
+target5.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/tasty-pie-slice-and-baked-pear-on-black-plate-6210838/" class="demo1" target="blank" rel="external nofollow">Image created by Tim Douglas - Pexels</a>` ;
+})
+
+const target6 = document.getElementById("target6");
+target6.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/pizzas-and-silverware-19968430/" class="demo1" target="blank" rel="external nofollow">Image created by Rene Strgar - Pexels</a>` ;
+})
+
+const target7 = document.getElementById("target7");
+target7.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/pizza-served-in-a-restaurant-20844860/" class="demo1" target="blank" rel="external nofollow">Image created by Nadin Sh - Pexels</a>` ;
+})
+
+const target8 = document.getElementById("target8");
+target8.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/blue-juice-with-mint-herb-3186752/" class="demo1" target="blank" rel="external nofollow">Image created by Nerfee Mirandilla - Pexels</a>` ;
+})
+
+const target9 = document.getElementById("target9");
+target9.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/woman-hand-holding-pizza-slice-19533168/" class="demo1" target="blank" rel="external nofollow">Image created by Giona Mason - Pexels</a>` ;
+})
+
+const target10 = document.getElementById("target10");
+target10.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/baked-pepperoni-pizza-cut-in-triangular-portions-10875220/" class="demo1" target="blank" rel="external nofollow">Image created by Valeria Boltneva - Pexels</a>` ;
+})
+
+const target11 = document.getElementById("target11");
+target11.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/fruit-alcoholic-cocktail-in-glass-12419161/" class="demo1" target="blank" rel="external nofollow">Image created by Denys Gromov - Pexels</a>` ;
+})
+
+const target12 = document.getElementById("target12");
+target12.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href="https://www.pexels.com/photo/classic-pizza-baked-and-cut-in-triangular-servings-10875297/" class="demo1" target="blank" rel="external nofollow">Image created by Valeria Boltneva - Pexels</a>` ;
+})
+
+const target13 = document.getElementById("target13");
+target13.addEventListener("click", () => {
+    document.getElementById("demo").innerHTML = `<a href=https://www.pexels.com/photo/salad-on-plate-17478678/" class="demo1" target="blank" rel="external nofollow">Image created by Nadin Sh - Pexels</a>` ;
+})
